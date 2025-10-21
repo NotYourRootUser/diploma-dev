7b779a91ab8e1242658c4d473e87ace51fc180d4
justin <justin.uaisele@gmail.com>
2025-10-22 11:18:47 +1300

    w2 d2 and d3 for mission ready folders

---
+++ week2/day2-clone-repo/my-first-repo/README.md
@@ -0,0 +1,3 @@
+# My First Repo
+
+Practice repository for learning Git and basic version control, following teachers steps during live.
---
+++ week2/day3-js-intro/scripts.js
@@ -0,0 +1,23 @@
+console.log("Hello world from external js file")
+
+// undefined and defining later becayse let allows
+let y;
+console.log(y)
+y= 10;
+console.log(y)
+
+// const z; (doesnt run becauase not defined)
+
+// let a = 1; indentifer 'a' has already been declared
+// let a = 2;
+
+// const b = 3; identifier 'b'has already been declared
+// const b = 4;
+
+let num= 5;
+console.log(num)
+num = 10; // reassignment is allowed
+console.log(num);       
+
+const a= 600;
+a= 200; // not allowed
