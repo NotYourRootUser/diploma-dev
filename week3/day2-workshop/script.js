b0cefeb9d4cba3da443fb4d4d35056f7df6bcf3f
justin <justin.uaisele@gmail.com>
2025-10-28 15:07:53 +1300

    day 2 workshop week 3

---
+++ week3/day2-workshop/script.js
@@ -0,0 +1,12 @@
+let num1 = 500;
+let num2 = 5;
+if (num1 > num2) {
+    console.log(`num1 is greater than num2`);
+} else {
+    console.log(`num1 is not greater than num2`);
+}
+
+
+let str1 = "apple";
+let str2 = "banana";
+str1 === str2 ? console.log("equal") : console.log("No");
