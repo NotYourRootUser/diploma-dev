ae0d43ae9b6bac2b7dcf25e7c98c813adae2f249
Justin <justin.uaisele@gmail.com>
2025-12-05 13:52:45 +1300

    revision for day5 modules

---
+++ week8/day5-reinforcement/index.js
@@ -0,0 +1,4 @@
+import { divNum, squareNum, isEven } from "./module.js";
+console.log(divNum(100,10));
+console.log(squareNum(20))
+console.log(isEven(5))
---
+++ week8/day5-reinforcement/module.js
@@ -0,0 +1,9 @@
+const divNum = (num1, num2) => num1 / num2;
+const squareNum = (num) => num * num;
+const isEven = (num) => num % 2 === 0;
+
+export {
+    divNum,
+    squareNum,
+    isEven,
+}
